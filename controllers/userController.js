231cd21257a752a9b29f7d3e82973030a5d96999
AkshandraSingh <nameste380@gmail.com>
2024-04-30 23:53:49 +0530

    Update: addedLoginUserAPI

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 const userModel = require('../models/userModel')
 
 module.exports = {
@@ -13,10 +16,46 @@ module.exports = {
                     message: "User Already Exist"
                 })
             }
+            const hashedPassword = await bcrypt.hash(req.body.userPassword, 10)
+            userData.userPassword = hashedPassword
             await userData.save()
             res.status(202).send({
                 success: true,
                 message: "User Registered Successfully"
+            })
+        } catch (error) {
+            res.status(500).send({
+                success: false,
+                message: "Error",
+                error: error.message
+            })
+        }
+    },
+
+    loginUser: async (req, res) => {
+        try {
+            const { userEmail, userPassword } = req.body
+            const isUserExist = await userModel.findOne({
+                userEmail: userEmail
+            })
+            if (!isUserExist) {
+                return res.status(400).send({
+                    success: false,
+                    message: "User Does Not Exist"
+                })
+            }
+            const isPasswordCorrect = await bcrypt.compare(userPassword, isUserExist.userPassword)
+            if (!isPasswordCorrect) {
+                return res.status(400).send({
+                    success: false,
+                    message: "Incorrect Password"
+                })
+            }
+            const token = jwt.sign({ isUserExist }, process.env.SECRET_KEY, { expiresIn: '1h' });
+            res.status(200).send({
+                success: true,
+                message: "User Logged In Successfully",
+                token: token
             })
         } catch (error) {
             res.status(500).send({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController')
 const userRouter = express.Router()
 
 userRouter.post('/registerUser', userController.registerUser)
+userRouter.post('/loginUser', userController.loginUser)
 
 module.exports = userRouter
