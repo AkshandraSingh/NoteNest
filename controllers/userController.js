9012238854f743eddffc42cbf2df6315e2f14a2c
AkshandraSingh <nameste380@gmail.com>
2024-05-01 11:06:39 +0530

    Update: addedSetNewPasswordAPI

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const userModel = require('../models/userModel')
 const emailService = require('../services/emailService')
 
 module.exports = {
+    // Register User API
     registerUser: async (req, res) => {
         try {
             const userData = new userModel(req.body)
@@ -33,6 +34,7 @@ module.exports = {
         }
     },
 
+    // Login User API
     loginUser: async (req, res) => {
         try {
             const { userEmail, userPassword } = req.body
@@ -67,6 +69,7 @@ module.exports = {
         }
     },
 
+    // Forget Password API
     forgetPassword: async (req, res) => {
         try {
             const { userEmail } = req.body
@@ -86,6 +89,41 @@ module.exports = {
                 message: "Email has been sended successfully",
                 userId: isEmailExist._id,
                 token: token,
+            })
+        } catch (error) {
+            res.status(500).send({
+                success: false,
+                message: "Error",
+                error: error.message
+            })
+        }
+    },
+
+    // Set New Password API
+    setNewPassword: async (req, res) => {
+        try {
+            const { userId, token } = req.params
+            const { newPassword, confirmPassword } = req.body
+            const userData = await userModel.findById(userId)
+            const isTokenValid = await jwt.verify(token, process.env.SECRET_KEY)
+            if (!isTokenValid) {
+                return res.status(400).send({
+                    success: false,
+                    message: "Token Expired"
+                })
+            }
+            if (newPassword !== confirmPassword) {
+                return res.status(400).send({
+                    success: false,
+                    message: "Passwords do not match"
+                })
+            }
+            const hashedPassword = await bcrypt.hash(newPassword, 10)
+            userData.userPassword = hashedPassword
+            await userData.save()
+            res.status(200).send({
+                success: true,
+                message: "Password Changed Successfully"
             })
         } catch (error) {
             res.status(500).send({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,5 +7,6 @@ const userRouter = express.Router()
 userRouter.post('/registerUser', userController.registerUser)
 userRouter.post('/loginUser', userController.loginUser)
 userRouter.post('/forgetPassword', userController.forgetPassword)
+userRouter.post('/setNewPassword/:userId/:token', userController.setNewPassword)
 
 module.exports = userRouter
